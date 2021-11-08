5304acaca308dd2a19442fa3826bdb41e3bbae52
[somilpatil] <[patilsomil118@gmail.com]>
2021-11-08 23:11:08 +0530

    Added: DeleteContacts

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -7,46 +7,61 @@ import java.util.Scanner;
 public class AddressBook {
 	static Scanner sc = new Scanner(System.in);
 	static List<Contact> list = new LinkedList<Contact>();
-	private static AddressBook editcontact;
-	
-	//Created method for adding contact
-	public static void addContact(){
-		System.out.println("Enter your firstName : ");
-		String firstName = sc.nextLine();
-		System.out.println("Enter your lastName : ");
-		String lastName = sc.nextLine();
-		System.out.println("Enter your address : ");
-		String address = sc.nextLine();
-		System.out.println("Enter your city : ");
-		String city = sc.nextLine();
-		System.out.println("Enter your state : ");
-		String state = sc.nextLine();
-		System.out.println("Enter your zipCode : ");
-		String zip = sc.nextLine();
-		System.out.println("Enter your phoneNo : ");
-		String phoneNo = sc.nextLine();
-		System.out.println("Enter your emailId : ");
-		String email = sc.nextLine();
-		Contact obj = new Contact(firstName, lastName, address, city, state, zip, phoneNo, email);
-		list.add(obj);
-	}
-	public static void editContact(){
-		//Scanner sc = new Scanner(System.in);
-		System.out.println("Enter first name: ");
-		String firstName = sc.nextLine();
-		for (int i = 0; i < list.size(); i++) {
-			if (list.get(i).getFirstName().equalsIgnoreCase(firstName)){
-				list.remove(i);
-				addContact();
-	        }
-			else {
-	                System.out.println("No data found in Address Book");
-	        }
-		}
-	}
-	public static void main(String[] args) {
+    private static AddressBook addressbook;
+    private static AddressBook addressbook2;
 
-		AddressBook editcontact = new AddressBook();
+    //Created method for adding contact
+    public static void addContact(){
+    	System.out.println("Enter your firstName : ");
+    	String firstName = sc.nextLine();
+    	System.out.println("Enter your lastName : ");
+    	String lastName = sc.nextLine();
+    	System.out.println("Enter your address : ");
+    	String address = sc.nextLine();
+    	System.out.println("Enter your city : ");
+    	String city = sc.nextLine();
+    	System.out.println("Enter your state : ");
+    	String state = sc.nextLine();
+    	System.out.println("Enter your zipCode : ");
+    	String zip = sc.nextLine();
+    	System.out.println("Enter your phoneNo : ");
+    	String phoneNo = sc.nextLine();
+    	System.out.println("Enter your emailId : ");
+    	String email = sc.nextLine();
+    	Contact obj = new Contact(firstName, lastName, address, city, state, zip, phoneNo, email);
+    	list.add(obj);
+    }
+
+
+    public static void editContact() {
+    	//Scanner sc = new Scanner(System.in);
+    	System.out.println("Enter first name: ");
+    	String firstName = sc.nextLine();
+    	for (int i = 0; i < list.size(); i++) {
+    		if (list.get(i).getFirstName().equalsIgnoreCase(firstName)){
+    			list.remove(i);
+    			addContact();
+    		}
+    		else {
+    			System.out.println("No data found in Address Book");
+    		}
+    	}
+    }
+    //Creating deleteContact() for deleting contact details using first name
+    public static void deleteContact() {
+    	System.out.println("Enter first name : ");
+	    String firstName = sc.nextLine();
+	    for (int i = 0; i < list.size(); i++) {
+	    	if (list.get(i).getFirstName().equalsIgnoreCase(firstName)) {
+	                list.remove(i);
+	        } 
+	    	else {
+	                System.out.println("No data found");
+	        }
+	    }
+    }
+    public static void main(String[] args) {
+		addressbook2 = new AddressBook();
 		//Displaying the welcome message
 		System.out.println("WELCOME TO ADDRESS BOOK PROBLEM");
 		//Adding new contact
@@ -54,6 +69,8 @@ public class AddressBook {
 		addContact();
 		//Editing Contact
 		editContact();
+		//deleting contact
+		deleteContact();
 		System.out.println(list); //printing list
 	}
 }
